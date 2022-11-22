ddc8ad407bedb6e5e762e0a46a54f2279e9c8cdd
saidshah <saidmshah93@gmail.com>
2022-11-22 13:40:31 -0600

    add arrow functions

--- js/functions.js
+++ js/functions.js
@@ -11,6 +11,12 @@
 function sayHello(name){
     return ("Hello " + name + ".");
 }
+
+//The arrow function version of the above function
+const sayHelloArrow =(name)=>{
+    return `Hello ${name}.`
+}
+console.log(sayHelloArrow("Alejandro"));
 /**
  * TODO:
  * Call the function 'sayHello' and pass your name as a string literal argument.
@@ -20,6 +26,10 @@ function sayHello(name){
  */
 let helloMessage = sayHello("Alex");
 console.log(helloMessage);
+
+//The arrow function version of the above function
+let helloMessageArrow = sayHelloArrow("Alejandro");
+console.log(helloMessageArrow);
 /**
  * TODO:
  * Store your name as a string in a variable named 'myName', and pass that
@@ -29,6 +39,8 @@ console.log(helloMessage);
 let myName = "Alex";
 console.log(sayHello(myName));
 
+let myNameArrow = "Alejandro";
+console.log(sayHelloArrow(myNameArrow));
 // Don't modify the following line, it generates a random number between 1 and 3
 // and stores it in a variable named random
 var random = Math.floor((Math.random() * 3) + 1);
@@ -52,9 +64,15 @@ var random = Math.floor((Math.random() * 3) + 1);
  */
 function isTwo(num){
     //console.log("Testing");
-    return (num === 2)    
+    return (num === 2);  
 }
 console.log(isTwo(random));
+
+//The arrow function version of the above function
+const isTwoArrow =(num) =>{
+    return (num === 2);
+}
+console.log(isTwoArrow(random));
 
 /**
  * TODO:
@@ -72,6 +90,12 @@ function calculateTip(tipPercent, cost){
     return cost * tipPercent;
 }
 console.log(calculateTip(0.20, 20));
+
+//The arrow function version of the above function
+const calculateTipArrow = (tipPercent, cost) =>{
+    return cost * tipPercent;
+}
+console.log(calculateTipArrow(0.20, 20));
 //console.log(calculateTip(0.25, 25.50)); // returns 6.375. Tested
 /**
  * TODO:
@@ -84,6 +108,9 @@ let theCost = prompt("Enter the cost.");
 let theTip = calculateTip(tipPercentage, theCost);
 console.log("Your tip amount is: $" + theTip);
 
+//The arrow function version of the above function
+let theTipArrow = calculateTipArrow(tipPercentage, theCost);
+console.log("Your tip amount is: $" + theTipArrow);
 /**
  * TODO:
  * Create a function named `applyDiscount`. This function should accept a price
@@ -103,3 +130,9 @@ function applyDiscount(discount, price){
 }
 console.log("The discounted price is: $"+applyDiscount(0.20, 20));
 console.log("The discounted price is: $"+applyDiscount(0.50, 100));
+
+//The arrow function version of the above function
+const applyDiscountArrow = (discount, price) => {
+    return price - (discount * price);
+}
+console.log("The discounted price is: $"+ applyDiscountArrow(0.50, 100));
---
+++ js/test.js
@@ -0,0 +1,4 @@
+function add(num1, num2){
+    let result = num1 + num2;
+}
+console.log(4, 6);
