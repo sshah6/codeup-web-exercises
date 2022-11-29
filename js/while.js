a76cd594784a9e4572bebcb8cb7da7a949949aa3
saidshah <saidmshah93@gmail.com>
2022-11-29 15:23:56 -0600

    do-while add

--- js/while.js
+++ js/while.js
@@ -1,9 +1,33 @@
 console.log("testing routes (while loop)");
 
 //(2)
-var i = 0;
-while(i < 128){
-    var result = i * 2;
+
+var j = 1;
+while( j < 20){
+   let result = Math.pow(2, j);
+    j++; 
     console.log(result);
-    i = i * 2;
+    if(result === 65536)
+    break; 
 }
+
+//(1) ###### do-while loop ######
+
+var allCones = Math.floor(Math.random() * (100 - 50) + 50);//ceating random number between 50 to 100 numbers
+console.log("You have " + allCones + " cones.");
+let i = 0;
+ do{
+    var soldCones = Math.floor(Math.random() * (5 - 1) + 1);//creating the number of sold cones each time
+
+    if(allCones === 0){
+        console.log("Yay! all cones are sold");
+        break;
+    }else if(allCones >= soldCones){
+        console.log(soldCones + " cone/cones were sold.");
+        allCones = allCones - soldCones;//subtracting the amount of sold cones from total cones
+        console.log("The remaining cones: " + allCones); 
+    }else if(allCones < soldCones){  
+        console.log("Cannot sell you " + soldCones+" I only have " + allCones);
+    }
+    i++;
+}while( i < 100)
