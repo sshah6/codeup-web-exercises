948b20534604cb984f8e7677e59b9322b2372d4e
saidshah <saidmshah93@gmail.com>
2022-11-30 08:48:03 -0600

    while done

--- js/while.js
+++ js/while.js
@@ -13,21 +13,21 @@ while( j < 20){
 
 //(1) ###### do-while loop ######
 
-var allCones = Math.floor(Math.random() * (100 - 50) + 50);//ceating random number between 50 to 100 numbers
+var allCones = Math.floor(Math.random() * (100 - 50) + 50);//creating random number between 50 to 100 numbers
 console.log("You have " + allCones + " cones.");
 let i = 0;
  do{
     var soldCones = Math.floor(Math.random() * (5 - 1) + 1);//creating the number of sold cones each time
 
     if(allCones === 0){
-        console.log("Yay! all cones are sold");
+        console.log("Yay! I sold them all. ;)");
         break;
     }else if(allCones >= soldCones){
         console.log(soldCones + " cone/cones were sold.");
         allCones = allCones - soldCones;//subtracting the amount of sold cones from total cones
         console.log("The remaining cones: " + allCones); 
     }else if(allCones < soldCones){  
-        console.log("Cannot sell you " + soldCones+" I only have " + allCones);
+        console.log("Cannot sell you " + soldCones+", I only have " + allCones +".");
     }
     i++;
 }while( i < 100)
